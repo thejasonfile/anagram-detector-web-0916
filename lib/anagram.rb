31d2d474e22ca1d44f86f019066dfa268dbad9bc
Jason Arnold <jason.arnold.00@gmail.com>
2016-09-29 15:15:23 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arr)
+    arr.each_with_object([]) do |word, obj|
+      obj << word if word.chars.sort == @word.chars.sort
+    end
+  end
+
+end
